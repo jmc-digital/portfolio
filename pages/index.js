151440ce37a6d52ef40ec50f1f530c7e6de0c02f
jmc-digital <jennifer@jennifermchase.com>
2026-03-06 10:32:15 -0800

    Create index.js for Home component

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <h1>Jennifer M. Chase</h1>
+        <p>Full-Stack Developer & Designer</p>
+      </section>
+    </main>
+  );
+}
